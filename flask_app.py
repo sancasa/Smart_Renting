1558fd0a18cd7cf394b055986656db2ba72d2c21
Smart Renting <148595360+sancasa@users.noreply.github.com>
2024-03-19 15:07:29 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -12,19 +12,19 @@ def hello():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    Tipo = str(request.form['Tipo'])
-    Dormitórios = int(request.form['Dormitórios'])
-    Banheiros = int(request.form['Banheiros'])
-    Garagens = int(request.form['Garagens'])
-    Bairro = str(request.form['Bairro'])
-    Suítes = int(request.form['Suítes'])
-    Mobiliado = str(request.form['Mobiliado'])
-    features = np.array([[Tipo, Dormitórios, Banheiros, Garagens, Bairro, Suítes, Mobiliado]])
-    df = pd.DataFrame(features, columns = ['Tipo', 'Dormitórios', 'Banheiros', 'Garagens', 'Bairro', 'Suítes', 'Mobiliado'])
+    Type = str(request.form['Type'])
+    Bedrooms = int(request.form['Bedrooms'])
+    Bathrooms = int(request.form['Bathrooms'])
+    Garages = int(request.form['Garages'])
+    Neighborhood = str(request.form['Neighborhood'])
+    Suites = int(request.form['Suites'])
+    Furnished = str(request.form['Furnished'])
+    features = np.array([[Type, Bedrooms, Bathrooms, Garages, Neighborhood, Suites, Furnished]])
+    df = pd.DataFrame(features, columns = ['Type', 'Bedrooms', 'Bathrooms', 'Garages', 'Neighborhood', 'Suites', 'Furnished'])
     prediction = model.predict(df)
     output = np.expm1(prediction[0])
     # return render_template('index.html', prediction_text=f'Valor calculado do aluguel: R${round(float(output),0)}')
-    return f"Valor calculado do aluguel: R${int(output)}"
+    return f"Calculated value: R${int(output)}"
 
 if __name__ == "__main__":
     app.run()
